499793793d5d3694d737daf2fea60dd2305eeb88
Daria2313 <tapocheksuga2003@gmail.com>
2022-04-02 14:16:21 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(increase(new int[] {10,20,30,40,50}));
+        System.out.print("введите количество дней: ");
+        int days=new Scanner(System.in).nextInt();
+
+        convert(days);
 
     }
-    static int increase(int[] a) {
-
-
-        for (int i=0; i<a.length; i++){
-            System.out.print(i+" - ");
-            System.out.println(a[i]);
-        }
-
-        System.out.println("\nУвеличение массива на 10% \n");
-
-        for (int k=0; k<a.length;k++){
-            a[k]=(int) (a[k]*0.50);
-            System.out.print(k+" - ");
-            System.out.println(a[k]);
-        }
-        return 0 ;
+    static void convert(int days){
+        if(days<=0) throw new IllegalArgumentException();
+        System.out.println("In"+days+"дни:"+days*24+"часы"+days*1440+"минуты,"+days*86400+"seconds.");
 
     }
-
 }
-
-
